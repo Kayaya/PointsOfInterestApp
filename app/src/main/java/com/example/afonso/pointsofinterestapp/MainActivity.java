e9a55c95e84838504701a35c2a7aee56faa83aab
afonso <tiagokayaya@hotmail.com>
2017-04-20 22:46:22 +0100

    Add OSM map

--- app/src/main/java/com/example/afonso/pointsofinterestapp/MainActivity.java
+++ app/src/main/java/com/example/afonso/pointsofinterestapp/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.afonso.pointsofinterestapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.preference.PreferenceManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.model.LatLng;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+
+public class MainActivity extends Activity {
+
+    MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /* Download OSM maps*/
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+
         setContentView(R.layout.activity_main);
+        initMap();
+    }
+
+    private void initMap(){
+        mapView = (MapView)findViewById(R.id.mapView);
+        mapView.setBuiltInZoomControls(true);
+        getNewLocationZoom(39.008224, -76.8984527, 15);
+    }
+
+    private void getNewLocationZoom(double latitude, double longitude, int zoom){
+        GeoPoint ll = new GeoPoint(latitude, longitude);
+        mapView.getController().setZoom(zoom);
+        mapView.getController().setCenter(ll);
+
     }
 }
