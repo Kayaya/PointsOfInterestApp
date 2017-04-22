0ca9129f5a89915ac3b16f753c98549643b2ef16
Kayaya <1kayat34@solent.ac.uk>
2017-04-22 21:36:02 +0100

    add menu and second activity...

--- app/src/main/java/com/example/afonso/pointsofinterestapp/MainActivity.java
+++ app/src/main/java/com/example/afonso/pointsofinterestapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.afonso.pointsofinterestapp;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -11,6 +12,9 @@ import android.preference.PreferenceManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -36,7 +40,7 @@ public class MainActivity extends Activity implements LocationListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /* Download OSM maps*/
+        // Download OSM maps
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
 
         setContentView(R.layout.activity_main);
@@ -61,20 +65,23 @@ public class MainActivity extends Activity implements LocationListener{
 
         }
 
-    }/*end of onCreate*/
 
+
+
+    }//end of onCreate
+    // Setting default values for the map when app is first launched
     private void initMap (){
         mapView.setBuiltInZoomControls(true);
         getNewLocationZoom(39.008224, -76.8984527, 15);
     }
-
+    //Get location and map zoom
     private void getNewLocationZoom(double latitude, double longitude, int zoom){
         GeoPoint ll = new GeoPoint(latitude, longitude);
         mapView.getController().setZoom(zoom);
         mapView.getController().setCenter(ll);
     }
 
-
+    //Setting location to user's location
     @Override
     public void onLocationChanged(Location location) {
         Toast.makeText(this, "Location=" +
@@ -100,5 +107,25 @@ public class MainActivity extends Activity implements LocationListener{
     public void onProviderDisabled(String provider) {
         Toast.makeText(this, "Provider" + provider + "disabled", Toast.LENGTH_LONG).show();
 
+    }
+    // Making the menu appear
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    //Reacting to a menu selection
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.add_poi){
+            //when item add_poi item selected launch second activity
+            Intent intent = new Intent (this, AddPOIActivity.class);
+            startActivity(intent);
+
+            return true;
+        }
+        //Otherwise do nothing
+        return false;
     }
 }
